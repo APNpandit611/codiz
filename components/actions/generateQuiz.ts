fe238cb9d47f4019affb7d35ac9a3081f1e4e91b
Abhishek Pandit <65335058+APNpandit611@users.noreply.github.com>
2025-11-17 21:25:43 +0200

    Update generateQuiz.ts

--- components/actions/generateQuiz.ts
+++ components/actions/generateQuiz.ts
@@ -68,10 +68,10 @@ Now, please generate a new coding challenge based on the instructions above. Mak
               "type": "image_url",
               "image_url": {
                 "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg"
-              }
-            }
-          ]
-        }
+              },
+            },
+          ],
+        },
       ],
         });
 
