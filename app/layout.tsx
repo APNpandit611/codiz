7415a24a491cd1c56c528ad89bbc4105d93e5626
APNpandit611 <apnpanditt@gmail.com>
2026-01-07 08:20:37 +0200

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,16 @@ import { ClerkProvider } from "@clerk/nextjs";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Quiz Generator App",
+    title: "Codiz",
     description: "A quiz app to practice coding skills",
+    icons: {
+    apple: '/apple-touch-icon.png',
+    icon: [
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
